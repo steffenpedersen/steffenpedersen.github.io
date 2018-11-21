1048c90556ca2d959122d80e2b547c42e6b1e0e4
steffenpedersen <steffen.pedersen@live.dk>
2018-11-21 19:17:48 +0100

    Add basic setup of using GitHub API

---
+++ _includes/main.js
@@ -0,0 +1,36 @@
+// TODO: This should be written into ES2015
+
+var container = document.querySelector('.c-open-source');
+
+// TODO: Add comments
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.github.com/users/steffenpedersen/repos?sort=updated', true);
+request.onload = function () {
+
+    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach(function (rep) {
+            // TODO: Filter out selected repositories
+
+            var card = document.createElement('div');
+            card.setAttribute('class', 'c-open-source__item');
+
+            var h3 = document.createElement('h3');
+            h3.textContent = rep.name;
+
+            var p = document.createElement('p');
+            p.textContent = rep.description;
+
+            container.appendChild(card);
+            card.appendChild(h3);
+            card.appendChild(p);
+        });
+    } else {
+        var errorMessage = document.createElement('marquee');
+        errorMessage.textContent = "Gah, it's not working!";
+        container.appendChild(errorMessage);
+    }
+}
+
+request.send();
